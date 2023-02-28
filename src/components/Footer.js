487768bdaad80c79f9895a38f4aa911a3eadb9cc
Ayush-0502 <ayush.shrivastav@kalvium.community>
2023-02-28 21:30:03 +0530

    updated react application

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,21 +13,21 @@ const Footer = () => {
           rel="noreferrer"
           href="https://www.instagram.com/Maverick_6041"
         >
-          <img src={instagram} height={20} alt="instagramLogo" />
+          <img src={instagram} height={25} alt="instagramLogo" />
         </a>
         <a
           target="_blank"
           rel="noreferrer"
           href="https://www.linkedin.com/in/ayush-kumar-346aa3248"
         >
-          <img src={LinkedIN} height={20} alt="Linkedin_Logo" />
+          <img src={LinkedIN} height={25} alt="Linkedin_Logo" />
         </a>
         <a
           target="_blank"
           rel="noreferrer"
           href="https://www.github.com/Ayush604141"
         >
-          <img src={Github} height={20} alt="Github_Logo" />
+          <img src={Github} height={25} alt="Github_Logo" />
         </a>
       </div>
     </footer>
