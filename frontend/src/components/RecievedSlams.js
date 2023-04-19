a32c1c02d514800b7616737360e5788b56604bcd
Ayush Shrivastav <ayush41red@gmail.com>
2023-04-19 13:12:37 +0530

    Tasks executed for technical interview.

--- frontend/src/components/RecievedSlams.js
+++ frontend/src/components/RecievedSlams.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 const RecievedSlams = () => {
+  const { user } = useAuthContext();
+  const [slamsRec, setSlamsRec] = useState([]);
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_API_URL + user.user._id)
+      .then((res) => {
+        setSlamsRec(res.data.received_slams);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, [user.user._id]);
   return (
-    <div>
-      
+    <div style={{ color: "white", marginTop: "10vh" }}>
+      {slamsRec.length === 0 ? (
+        <h1>No slams received</h1>
+      ) : (
+        slamsRec.map((item) => {
+          return <h1>{item.name}</h1>;
+        })
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default RecievedSlams
+export default RecievedSlams;
