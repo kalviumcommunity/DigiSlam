e0efbdff1bb9d352c3ff51471fb5c9ea31686394
Ayush604141 <112609589+Ayush604141@users.noreply.github.com>
2023-05-08 10:02:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const UserRoutes = require("./routes/UserRoutes");
 const cors = require("cors");
 const PORT = process.env.PORT;
 const MONGODB_TEST = process.env.MONGODB_TEST;
-const MONGODB_URL = process.env.MONGOSB_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
